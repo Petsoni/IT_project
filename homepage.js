88e21c8b2e44521c5a7094563ce8b7f9a5bc8bf9
Petar` <petar.markvovic.4977@metropolitan.ac.rs>
2021-11-18 13:03:03 +0100

    init commit

---
+++ homepage.js
@@ -0,0 +1,16 @@
+$("button").click(function () {
+    $('html,body').animate({
+            scrollTop: $(".news").offset().top
+        },
+        'slow');
+});
+
+var navLinks = document.getElementById("navLinks");
+
+function openMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
