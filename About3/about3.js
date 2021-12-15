c85325b5b5c19bcbe5a494b431ec7b258645a70e
Petar` <petar.markvovic.4977@metropolitan.ac.rs>
2021-12-15 15:54:27 +0100

    Form field added for message, needs more work

--- About3/about3.js
+++ About3/about3.js
@@ -2,21 +2,18 @@ function goBack() {
   window.history.back();
 }
 
-const section = document.querySelector('section');
-const imgContainer = document.createElement('div');
-
-function displayImg(imgUrl){
-
-  section.appendChild(imgContainer);
-
-  imgContainer.innerHTML = `<img src = '$ {imgUrl}'>`;
-
-  imgContainer.classList.add('displayMe');
-  //
-  // imgContainer.style.top = (elt.offsetTop + 40) + 'px';
-  // imgContainer.style.left = (elt.offsetLeft + 40) + 'px';
-
-
-function removeImg(){
-  body.removeChild(imgContainer);
-}
+// const section = document.querySelector('section');
+// const imgContainer = document.createElement('div');
+//
+// function displayImg(imgUrl){
+//
+//   section.appendChild(imgContainer);
+//
+//   imgContainer.innerHTML = `<img src = '$ {imgUrl}'>`;
+//
+//   imgContainer.classList.add('displayMe');
+//
+//
+// function removeImg(){
+//   body.removeChild(imgContainer);
+// }
