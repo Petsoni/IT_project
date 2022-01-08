edf1fdf5de924f41870600663fabf4cb49aca6aa
Petsoni <petar.markovic.4977@metropolitan.ac.rs>
2022-01-08 23:21:50 +0100

    Fully finished

--- Signup/signup.js
+++ Signup/signup.js
@@ -2,14 +2,27 @@ let x = document.getElementById("login");
 let y = document.getElementById("register");
 let z = document.getElementById("btn");
 
-function register(){
+function register() {
     x.style.left = "-400px"
     y.style.left = "50px"
     z.style.left = "110px"
 }
 
-function login(){
+function login() {
     x.style.left = "50px"
     y.style.left = "450px"
     z.style.left = "0"
+}
+
+function validation() {
+    let mail = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (mail === "pmarkovic2308@gmail.com" && password === "12345") {
+        alert('Logged in successfully');
+        document.getElementById('email').value = "";
+        document.getElementById('password').value = "";
+    } else {
+        alert('Incorrect email or password!')
+    }
 }
